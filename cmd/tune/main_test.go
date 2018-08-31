345343adc6dbf45e6157ed21933b3c5fed44e851
David Tolpin <david.tolpin@gmail.com>
2018-08-31 11:19:01 +0300

    tests for inferData

--- cmd/tune/main_test.go
+++ cmd/tune/main_test.go
@@ -16,7 +16,7 @@ func TestReadData(t *testing.T) {
 8
 6
 10`
-    for _, c := range []struct{
+    for i, c := range []struct{
         from, till int
         counts []int
     }{{0, 1000, []int{10, 8, 7, 9, 5, 8, 6, 10}},
@@ -26,8 +26,9 @@ func TestReadData(t *testing.T) {
         rdr := csv.NewReader(strings.NewReader(in))
         counts := readData(rdr, c.from, c.till)
         if(len(counts) != len(c.counts)) {
-            t.Errorf("Wrong number of counts (%v) for from=%d, till=%d: got %d, want %d",
-                counts, c.from, c.till, len(counts), len(c.counts))
+            t.Errorf("readData(%d): wrong number of counts (%v) " +
+                "for from=%d, till=%d: got %d, want %d",
+                i, counts, c.from, c.till, len(counts), len(c.counts))
         }
         for i := 0; i != len(c.counts); i++ {
             if counts[i] != c.counts[i] {
@@ -39,4 +40,21 @@ func TestReadData(t *testing.T) {
 }
 
 func TestInferBandwidth(t *testing.T) {
+    for i, c := range []struct{
+        total int
+        bandwidth float64
+        counts []int
+        walk float64
+        N int
+    }{
+        {10, 10000, []int{1, 1, 1, 1, 1, 1, 1, 1}, 10, 1000},
+        {10, 10000, []int{1, 10, 1, 10, 1, 10, 1, 10}, 10, 1000},
+    } {
+        mean, std := inferBandwidth(c.total, c.bandwidth, c.counts,
+            c.walk, c.N)
+        if mean < std {
+            t.Errorf("inferBandwidth(%d) mean (%.3f) should be greated than std (%.3f)",
+                i, mean, std)
+        }
+    }
 }
